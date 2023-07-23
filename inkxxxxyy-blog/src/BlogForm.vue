29597cb528902f8f7b33cb18ecb2f4999ed4d4dc
Your Name <you@example.com>
2023-07-24 01:11:04 +0800

    静态布局

---
+++ inkxxxxyy-blog/src/BlogForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>创建新博客</h1>
+    <form @submit.prevent="createBlog">
+      <div>
+        <label for="title">标题:</label>
+        <input type="text" id="title" v-model="title" required />
+      </div>
+      <div>
+        <label for="content">内容:</label>
+        <textarea id="content" v-model="content" required></textarea>
+      </div>
+      <button type="submit">创建</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '', // 博客标题
+      content: '' // 博客内容
+    };
+  },
+  methods: {
+    createBlog() {
+      // 发送创建博客的请求的逻辑
+      // 可以使用 axios 或者其他 HTTP 请求库发送 POST 请求
+      // 将 this.title 和 this.content 作为请求体发送到后端
+      // 处理成功后，可以重置表单字段
+    }
+  }
+};
+</script>
---
+++ inkxxxxyy-blog/src/Home.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<p>Hello</p>
+		<p>我是黄巧仪，这是我的个人网站。 </p>
+		<p>很高兴你来到这里, 你可以在以下的网站找到我</p>
+
+		<ul>
+			<li> <a href="https://github.com/INKXXXXYY">GitHub（整理中）</a> </li>
+			<li> <a href="https://blog.csdn.net/weixin_52448178">CSDN博客(写的比较多，也在整理中)</a> </li>
+		</ul>
+	</div>
+	<div>
+		<h1>博客列表</h1>
+		<ul>
+			<li v-for="blog in blogs" :key="blog.id">
+				<h2>{{ blog.title }}</h2>
+				<p>{{ blog.content }}</p>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomePage',
+
+		data() {
+			return {
+				blogs: [] // 博客列表数据
+			};
+		},
+		mounted() {
+			// 在组件挂载后，获取博客列表数据
+			this.fetchBlogs();
+		},
+		methods: {
+			fetchBlogs() {
+				// 从后端获取博客列表数据的逻辑
+				// 可以使用 axios 或者其他 HTTP 请求库发送请求
+				// 并将返回的数据赋值给 this.blogs
+			}
+		}
+	};
+</script>
