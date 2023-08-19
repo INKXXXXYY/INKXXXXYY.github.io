38d3e6f243b0481c78366a50a88127baa36a7001
Your Name <you@example.com>
2023-08-19 17:05:35 +0800

    static vue4

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import './assets/main.css'
 // import Vue from 'vue'
 
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
 import App from './App.vue'
 // import router from './router'
 
 // Vue.use(router)
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
