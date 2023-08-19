a1f8a742a8da712e8524e176a466ab31e9aa9d78
Your Name <you@example.com>
2023-08-19 17:29:25 +0800

    static vue5

--- src/router/index.js
+++ src/router/index.js
@@ -6,34 +6,34 @@ import blog from '@/components/blog'
 Vue.use(Router)
 
 export default new Router({
-	routes: [{
-			path: '/home',
-			name: 'home',
-			component: home
-		},
-		{
-			path: '/blog',
-			name: 'blog',
-			component: blog
-		},
-		// 容错机制
-		{
-			path: '*',
-			name: 'error',
-			redirect: '/home',
-			component: home
-		},
-		{
-			path: '/',
-			name: 'home-default',
-			redirect: '/home',
-			component: home
-		},
-		// 携带参数
-		{
-			path: '/blog/display/:id',
-			name: '/blog/display',
-			component: display
-		}
-	]
+	// routes: [{
+	// 		path: '/home',
+	// 		name: 'home',
+	// 		component: home
+	// 	},
+	// 	{
+	// 		path: '/blog',
+	// 		name: 'blog',
+	// 		component: blog
+	// 	},
+	// 	// 容错机制
+	// 	{
+	// 		path: '*',
+	// 		name: 'error',
+	// 		redirect: '/home',
+	// 		component: home
+	// 	},
+	// 	{
+	// 		path: '/',
+	// 		name: 'home-default',
+	// 		redirect: '/home',
+	// 		component: home
+	// 	},
+	// 	// 携带参数
+	// 	{
+	// 		path: '/blog/display/:id',
+	// 		name: '/blog/display',
+	// 		component: display
+	// 	}
+	// ]
 })
